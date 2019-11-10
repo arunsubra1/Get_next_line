5eddaf9bd4c486f7c5b7f9f34f56d99d3b86faf7
arun subra <arun.subra.1@gmail.com>
2019-11-10 01:55:53 -0800

    commented

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,19 @@
 /*   By: asubrama <asubrama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 16:37:21 by asubrama          #+#    #+#             */
-/*   Updated: 2019/11/10 01:22:04 by asubrama         ###   ########.fr       */
+/*   Updated: 2019/11/10 01:51:02 by asubrama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/*
+**	we search for a '\n' character if the string assed does not carry a
+**	'\n' then as long as it is the string we free the memory and return
+**	as read. if it does have a newline then while the string is yet to
+**	be '\0' we read line by line storing each line temporarily then
+**	freeing it and marking as read otherwise we 
+*/
 
 static int			ft_next_line(char **s, char **line, int fd)
 {
@@ -41,7 +49,10 @@ static int			ft_next_line(char **s, char **line, int fd)
 }
 
 /*
-**	store contents of file into large string (buff)
+**	BUFFSIZE is the int length we want to read up to and then set a '\n'.
+**	Store contents of file into large string (buff). create a new string
+**	size of 0. join new string with buff and set equal to tmp string.
+**	free str and set it equal to tmp and free tmp.
 */
 
 int					get_next_line(int const fd, char **line)
